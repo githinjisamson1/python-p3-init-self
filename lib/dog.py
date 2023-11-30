ae407dd415446e53c013422ceac944e23916349e
githinjisamson1 <githinjisamson148@gmail.com>
2023-11-30 04:47:10 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
+# class Dog:
+#     def __init__(self, name, favorite_toy="Any"):
+#         # print(f"{name} is born!")
+#         self.name = name
+#         self.favorite_toy = favorite_toy
+
+        
+#     def get_adopted(self, owner_name):
+#         self.owner = owner_name
+
+
+# fido = Dog("Fido")
+# fido.get_adopted("Doe")
+# print(fido.name)
+# print(fido.owner)
+# print(fido.favorite_toy)
+
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+        
+
